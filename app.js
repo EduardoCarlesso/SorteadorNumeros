0f1c7fafe3334a7e750c70ff0b4d48484224abe4
EduardoCarlesso <eduardocarlesso@gmail.com>
2026-02-15 16:17:45 -0300

    Logica inicial botao

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ function sortear(){
     let numeroMaximo = parseInt(document.getElementById("ate").value);
     let numeroSorteado;
     let sorteados =[];
-    let controle = 0
-
     for(i = 0; i < qtdNumerosSorteados; i++){
         numeroSorteado = sorteiaNumeros(numeroMinimo, numeroMaximo);
 
@@ -17,9 +15,29 @@ function sortear(){
     }
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = "Números sorteados: " + sorteados;
-
+    habilitarBotaoReinicio();
 }
 
 function sorteiaNumeros( min, max){
     return sorteio = parseInt(Math.random() * ((max+1) - min) + min);   
+}
+
+function habilitarBotaoReinicio(){
+    let botao = document.getElementById("btn-reiniciar");
+    if(botao.classList.contains("container__botao-desabilitado")){
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+    }else{
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+function reiniciar(){
+    let botao = document.getElementById("btn-reiniciar");
+    if(botao.classList.contains("container__botao")){ 
+        let qtdNumerosSorteados = document.getElementById("quantidade").value = "";
+        let numeroMinimo = document.getElementById("de").value = "";
+        let numeroMaximo = document.getElementById("ate").value = "";
+    }
 }
