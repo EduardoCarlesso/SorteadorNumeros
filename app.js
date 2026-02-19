6bae67dc140dbf126b185145fc82bc341d56ae84
EduardoCarlesso <eduardocarlesso@gmail.com>
2026-02-19 19:31:44 -0300

    Botao reiniciar arrumado

--- app.js
+++ app.js
@@ -47,19 +47,18 @@ function exibirSorteados(sorteados) {
 
 function alteraStatusBotaoReiniciar() {
     let botao = document.getElementById("btn-reiniciar");
-    if (botao.classList.contains("container__botao-desabilitado")) {
-        botao.classList.remove("container__botao-desabilitado");
-        botao.classList.add("container__botao");
-    } else {
-        botao.classList.remove("container__botao");
-        botao.classList.add("container__botao-desabilitado");
+    if(botao.hasAttribute("disabled")){
+        botao.removeAttribute("disabled");
+    }else{
+        botao.setAttribute("disabled", true);
     }
 }
+
 
 function reiniciar() {
     document.getElementById("quantidade").value = "";
     document.getElementById("de").value = "";
     document.getElementById("ate").value = "";
-    document.getElementById("resultado").value = "";
+    document.getElementById("resultado").innerHTML = "";
     alteraStatusBotaoReiniciar();
 }
